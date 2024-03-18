0a5f8df0b046a194f0d463c578fff50be5a153d1
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2024-03-17 22:01:48 -0400

    u

--- server/routes/app.js
+++ server/routes/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'dist/cms/broswer/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
 });
 
 module.exports = router;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl:   'https://wdd430-w11-cms-main.vercel.app',
-  localUrl: 'http://localhost:3000'
+  apiUrl:   'https://wdd430-w11-cms-main.vercel.app'
 };
