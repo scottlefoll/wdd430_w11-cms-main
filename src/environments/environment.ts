164217bec634157b3ffa4f0e4141ef0d9ae678b3
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2024-03-18 19:13:15 -0400

    U

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl:   'https://wdd430-w11-cms-main.vercel.app/api',
-  api2Url:  'https://wdd430-w11-cms-backend.vercel.app/api',
+  api2Url:   'https://wdd430-w11-cms-main.vercel.app/api',
+  apiUrl:  'https://wdd430-w11-cms-backend.vercel.app/api',
   localUrl: 'http://localhost:3000/api'
 };
